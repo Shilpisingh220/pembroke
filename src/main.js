5473e8c19286d1d919ead13b07d72c14396ca896
Shilpisingh220 <shilpibssingh27@gmail.com>
2024-10-28 13:45:38 +0000

    image src done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-console.log("Connected");
+import {capitalize} from "./utils"
 
 
 
@@ -6,11 +6,12 @@ console.log("Connected");
 // https://dog.ceo/api/breed/Affenpinscher/images/random = single image
 
 const selectEl = document.querySelector("select");
+const imgE1 = document.querySelector("img");
 
 // API
 const BASE_URL = `https://dog.ceo/api/`;
 
-
+// MARK: FETCH
 function getDogBreed() {
  return fetch(`${BASE_URL}breeds/list/all`)
 .then((res) => res.json())
@@ -19,16 +20,26 @@ function getDogBreed() {
     return Object.keys(data.message);
     
 })
-.catch((error) => console.log(error));
+ .catch((error) => console.log(error));
 }
 
+function getSingleImage(breed) {
+    return fetch(`${BASE_URL}breed/${breed}/images/random`)
+    .then((res) => res.json())
+    .then((data) => (data.message))
+    .catch((error) => console.log(error));
+}
 
+// getSingleImage("husky");
+
+// MARK: RENDER
 function renderOptiins() {
     getDogBreed().then((data) => {
         const fragment = document.createDocumentFragment();
         for(let breed of data){
             const option = document.createElement("option");
-            option.textContent = breed;
+            option.textContent = capitalize(breed);
+            option.value = breed;
             fragment.appendChild(option);
         }
         selectEl.appendChild(fragment);
@@ -43,3 +54,10 @@ renderOptiins();
 // .then()
 // .then()
 // .catch()
+
+// CHANGE ON USER SELECT
+selectEl.addEventListener("change", (event) => {
+    getSingleImage(event.target.value).then((data) => {
+        imgE1.src = data;
+    });
+});
